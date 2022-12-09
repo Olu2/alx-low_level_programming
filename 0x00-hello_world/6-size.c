4e756d278dcf5589a2ea756807e2465b1a95b797
Olu2 <ajogbejeo@gmail.com>
2022-12-08 16:35:37 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+/**
+ * main - print out sizes of data types in c
+ * code by mide c
+ * Returned 0
+ */
+int main(void)
+{ char a;
+  int b;
+  long int c;
+  long long int d;
+  float f;
+      printf("size of char:%mibyte(s)\n",(unsigned long size of (a)
+      printf("size of int:%mibyte(s)\n",(unsigned long size of (b)
+     printf("size of long int:%mibyte(s)\n",(unsigned long size of (c)
+       printf("size of long long int :%mibyte(s)\n",(unsigned long size of (d)
+  returned (0)
+  }  
